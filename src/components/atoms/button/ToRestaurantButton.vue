7510f670f12baecefb8264008c9bd988dee884cc
zwmimi <zwmimi1212@gmail.com>
2022-12-01 11:14:24 +0900

    一覧に戻るボタンの実装

---
+++ src/components/atoms/button/ToRestaurantButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-row align-content="center">
+      <v-col align="center">
+        <v-btn @click="toTop" color="amber accent-3" dark>一覧へ戻る</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ToTopButton",
+  methods: {
+    toTop() {
+      this.$router.push({ name: "restaurant" });
+    },
+  },
+};
+</script>
